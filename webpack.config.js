1d50b2b10c78d87c1ffc590763d4a7dad1852a5d
J Lopes <offeringofpie@gmail.com>
2018-03-26 21:56:47 +0200

    initial preact implementation

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: '/node_modules/'
       }, {
